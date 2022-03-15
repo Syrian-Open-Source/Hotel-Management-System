e7b3616d2e251412d3e0f04876afdcf5526206e1
karam  mustafa <karam2mustafa@gmail.com>
2022-03-15 22:16:49 +0200

    wip

--- app/Http/Controllers/Review/RateController.php
+++ app/Http/Controllers/Review/RateController.php
@@ -74,7 +74,9 @@ class RateController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
+     *
      * @return \Illuminate\Http\Response
+     * @throws \Illuminate\Validation\ValidationException
      */
     public function update(Request $request, $id)
     {
@@ -82,17 +84,10 @@ class RateController extends Controller
             'rate'  => 'required|integer|in:1,2,3,4,5'
         ]);
 
-        $input = $request->all();
+        $rate = Rate::findOrFail($id)->update($request->all());
 
-        $rate = Rate::find($id);
-        $rate->update($input);
-
-        $room_rate = Rate::avg('rate')->where('room_id',$rate->room_id);
-
-        $room = Room::where('id', $rate->room_id);
-
-        $room->update([
-            'rate' => $room_rate,
+        Room::where('id', $rate->room_id)->update([
+            'rate' => Rate::avg('rate')->where('room_id',$rate->room_id),
         ]);
 
         return response(['Message:'=>'Rate info edited successfully','Code:'=>'1','Rate' => $rate], 200);
